3e00db6b7caec63487324acd467c67fdb781da78
Rania244 <raniaa7med2092017@gmail.com>
2024-06-12 10:19:41 +0300

    rania

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ const Stack = createNativeStackNavigator();
 // ------------------Drawer Navigation------------
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
+import { CartContext, CartProvider } from "./screens/CartContext";
+
 import { Provider} from "react-redux";
 import Cart from "./screens/Cart";
 import Login from "./screens/Login";
@@ -16,6 +18,8 @@ import Register from "./screens/Register";
 import Search from "./screens/Search";
 import HomeScreen from "./screens/HomeScreen";
 import ProductDetails from "./screens/ProductDetails";
+import Checkout from "./screens/Checkout";
+
 const Drawer = createDrawerNavigator();
 
 
@@ -23,8 +27,8 @@ const MainStack = () => {
   return (
     <Stack.Navigator> 
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
-      <Stack.Screen name="ProductDetails"
-      component={ProductDetails} />    
+      <Stack.Screen name="ProductDetails" component={ProductDetails} />  
+      <Stack.Screen name="Checkout" component={Checkout} />  
     </Stack.Navigator> 
   )
 }
@@ -32,6 +36,16 @@ const MainStack = () => {
 const App = () => {
   return (
     
+        // <NavigationContainer> 
+        //   <Drawer.Navigator>
+        //     <Drawer.Screen name="Amazon" component={MainStack} />
+        //     <Drawer.Screen name="Search" component={Search} />
+        //     <Drawer.Screen name="Register" component={Register} />
+        //     <Drawer.Screen name="Login" component={Login} />
+        //     <Drawer.Screen name="Cart" component={Cart} />
+        //   </Drawer.Navigator>
+        // </NavigationContainer>
+        <CartProvider>
         <NavigationContainer> 
           <Drawer.Navigator>
             <Drawer.Screen name="Amazon" component={MainStack} />
@@ -39,10 +53,14 @@ const App = () => {
             <Drawer.Screen name="Register" component={Register} />
             <Drawer.Screen name="Login" component={Login} />
             <Drawer.Screen name="Cart" component={Cart} />
+            <Drawer.Screen name="CartContext" component={CartContext} />
           </Drawer.Navigator>
         </NavigationContainer>
+      </CartProvider>
     
   )
 }
 export default App;
+
+// App.js
 
